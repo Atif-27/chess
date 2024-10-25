e692d307528b229903e7c4e0ccbdcdb0a34b20e9
Atif Ali <atif276ali@gmail.com>
2024-10-26 01:32:49 +0530

    refactor(game): Refactor Game class and GameManager class

--- apps/game_socket_server/src/Game.ts
+++ apps/game_socket_server/src/Game.ts
@@ -1,57 +1,95 @@
 import WebSocket from "ws";
 import { Chess } from "chess.js";
-import { gameStarted, showGameOver, showMove } from "./helper/SocketPayload";
+import {
+  showGameCreated,
+  showGameOver,
+  showMove,
+} from "./helper/SocketPayload";
 import { TgameMove } from "@chess/types/types";
-export default class Game {
-  public player1: WebSocket;
-  public player2: WebSocket;
-  private board: Chess;
-  private startTime: Date;
+import prisma from "@chess/db/client";
+import { User } from "./UserManager";
 
-  constructor(player1: WebSocket, player2: WebSocket) {
-    this.player1 = player1;
-    this.player2 = player2;
+export default class Game {
+  public gameId: string;
+  public whitePlayerId: string;
+  public blackPlayerId: string;
+  public board: Chess;
+  public startTime: Date;
+
+  constructor(whitePlayerId: string, blackPlayerId: string) {
+    this.gameId = ""; // GameId is the id of the game in the database
+    this.whitePlayerId = whitePlayerId;
+    this.blackPlayerId = blackPlayerId;
     this.board = new Chess();
     this.startTime = new Date();
-
-    // Emiting to client that Game Started
-    this.player1.send(gameStarted({ color: "white" }));
-    this.player2.send(gameStarted({ color: "black" }));
     console.log("Game Created");
+    this.createGameInDB(); // Create Game in DB and get the gameId
   }
-  public makeMove(socket: WebSocket, move: TgameMove) {
-    //TODO: validation here with zod
-    //TODO: Also send checkmate or not
-    // ! Check Users Turn
-    const currentTurn = this.board.turn();
-    if (currentTurn === "w" && socket !== this.player1) return;
-    if (currentTurn === "b" && socket !== this.player2) return;
+  // public makeMove(socket: WebSocket, move: TgameMove) {
+  //   //TODO: validation here with zod
+  //   //TODO: Also send checkmate or not
+  //   // ! Check Users Turn
+  //   const currentTurn = this.board.turn();
+  //   if (currentTurn === "w" && socket !== this.player1) return;
+  //   if (currentTurn === "b" && socket !== this.player2) return;
 
-    //! Update  Board
-    try {
-      this.board.move({
-        from: move.from as string,
-        to: move.to as string,
-        promotion: "q",
-      });
-    } catch (error) {
-      console.error("Move Error:", error);
+  //   //! Update  Board
+  //   try {
+  //     this.board.move({
+  //       from: move.from as string,
+  //       to: move.to as string,
+  //       promotion: "q",
+  //     });
+  //   } catch (error) {
+  //     console.error("Move Error:", error);
+  //     return;
+  //   }
+  //   //! Check if game over
+  //   if (this.board.isGameOver()) {
+  //     console.log("CheckMate bro");
+  //     const winner = this.board.turn() === "w" ? "black" : "white";
+  //     //TODO Also Show return the move if rest of the code doesnt work
+  //     this.player1.send(showGameOver({ winner }));
+  //     this.player2.send(showGameOver({ winner }));
+  //     return;
+  //   }
+
+  //   //! send updated to both player
+  //   this.player1.send(showMove({ move }));
+  //   this.player2.send(showMove({ move }));
+
+  //   console.log("Move Made");
+  // }
+  async createGameInDB() {
+    const game = await prisma.game.create({
+      data: {
+        whitePlayer: {
+          connect: { id: this.whitePlayerId },
+        },
+        blackPlayer: {
+          connect: { id: this.blackPlayerId },
+        },
+      },
+    });
+    this.gameId = game.id;
+  }
+  public showGameCreated(users: User[]) {
+    const whitePlayer = users?.find(
+      (user) => user.userId === this?.whitePlayerId
+    );
+    const blackPlayer = users?.find(
+      (user) => user.userId === this?.blackPlayerId
+    );
+    //TODO: Check DB for game and send the game state
+    if (!whitePlayer || !blackPlayer) {
+      console.error("No users found");
       return;
     }
-    //! Check if game over
-    if (this.board.isGameOver()) {
-      console.log("CheckMate bro");
-      const winner = this.board.turn() === "w" ? "black" : "white";
-      //TODO Also Show return the move if rest of the code doesnt work
-      this.player1.send(showGameOver({ winner }));
-      this.player2.send(showGameOver({ winner }));
-      return;
-    }
-
-    //! send updated to both player
-    this.player1.send(showMove({ move }));
-    this.player2.send(showMove({ move }));
-
-    console.log("Move Made");
+    users?.forEach((user) => {
+      user.socket.send(
+        showGameCreated({ game: this, whitePlayer, blackPlayer })
+      );
+    });
   }
 }
+
--- apps/game_socket_server/src/GameManager.ts
+++ apps/game_socket_server/src/GameManager.ts
@@ -2,56 +2,102 @@ import WebSocket from "ws";
 import Game from "./Game";
 import { TgameMove } from "@chess/types/types";
 import { messages } from "@chess/types/messages";
+import PrismaClient from "@chess/db/client";
+import { User } from "./UserManager";
+import { gameCreated, showGameCreated } from "./helper/SocketPayload";
 
 class GameManager {
   private games: Game[];
-  private users: WebSocket[];
-  private pending: WebSocket | null;
+  private users: User[];
+  private usersInRoom: Map<string, User[]>;
+  private userRoomMapping: Map<string, string>;
+  private pendingUser: User | null;
   private static instance: GameManager;
   private constructor() {
     this.games = [];
     this.users = [];
-    this.pending = null;
+    this.usersInRoom = new Map<string, User[]>();
+    this.userRoomMapping = new Map<string, string>();
+    this.pendingUser = null;
   }
   static createGameManager(): GameManager {
     if (!GameManager.instance) GameManager.instance = new GameManager();
     return GameManager.instance;
   }
-  addUser(socket: WebSocket) {
-    this.users.push(socket);
-    console.log("User Added");
+  addUser(user: User) {
+    this.users.push(user);
+    this.addHandler(user);
+    console.log("User Added and Listening for Messages");
   }
   removeUser(socket: WebSocket) {
-    this.users.filter((user) => user !== socket);
+    const user = this.users.find((user) => user.socket === socket);
+    if (!user) {
+      console.error("User not found?");
+      return;
+    }
+    this.users = this.users.filter((user) => user.socket !== socket);
+    // socketManager.removeUser(user);
     //TODO Show game results as user disconnected
   }
-  addHandler(socket: WebSocket) {
-    // Server is basically Listening for any message from client
-    socket.on("message", (data) => {
+  broadcastMessage(room: string, message: string) {
+    const users = this.usersInRoom.get(room);
+    if (!users) {
+      console.error("No users in room");
+      return;
+    }
+    users?.forEach((user) => {
+      user.socket.send(message);
+    });
+  }
+  addHandler(user: User) {
+    //! Server is basically Listening for any message from client
+    user.socket.on("message", (data) => {
       const message = JSON.parse(data.toString());
       if (message.type === messages.INIT_GAME) {
-        this.initGame(socket);
+        this.initGame(user);
       }
       if (message.type === messages.MAKE_MOVE) {
-        this.moveInGame(socket, message.move);
+        // this.moveInGame(user.socket, message.move);
+      }
+      if (message.type === messages.SHOW_GAME_CREATED) {
+        this.showGameCreated(message.payload.gameId);
       }
     });
   }
-  initGame(socket: WebSocket) {
-    if (this.pending) {
-      const newGame = new Game(this.pending, socket);
-      this.games.push(newGame);
-      this.pending = null;
+  initGame(user: User) {
+    //TODO: Check if user is already in a game
+    if (!this.pendingUser) {
+      this.pendingUser = user;
     } else {
-      this.pending = socket;
+      const pendingUser = this.pendingUser;
+      const newGame = new Game(pendingUser.userId, user.userId);
+      this.games.push(newGame);
+      this.pendingUser = null;
+      this.usersInRoom.set(newGame.gameId, [pendingUser, user]);
+      this.broadcastMessage(
+        newGame.gameId,
+        gameCreated({ gameId: newGame.gameId })
+      );
     }
+    //TODO: Emiting to client that Game Started
+    // this.player1.send(gameStarted({ color: "white" }));
+    // this.player2.send(gameStarted({ color: "black" }));
   }
-  moveInGame(socket: WebSocket, move: TgameMove) {
-    const game = this.games.find(
-      (game) => game.player1 === socket || game.player2 === socket
-    );
-    game?.makeMove(socket, move);
+  showGameCreated(gameId: string) {
+    const users = this.usersInRoom.get(gameId);
+    const game = this.games.find((game) => game.gameId === gameId);
+    if (!users || !game) {
+      console.error("No users or game found");
+      return;
+    }
+    game?.showGameCreated(users);
   }
+  // moveInGame(socket: WebSocket, move: TgameMove) {
+  //   const game = this.games.find(
+  //     (game) => game.player1 === socket || game.player2 === socket
+  //   );
+  //   game?.makeMove(socket, move);
+  // }
 }
 
 export default GameManager;
