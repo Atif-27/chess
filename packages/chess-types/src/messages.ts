3037bdd972d4987d5601085d982324c9447c4ef0
Atif Ali <atif276ali@gmail.com>
2024-10-26 01:31:10 +0530

    feat(game): Update game message names

--- packages/chess-types/src/messages.ts
+++ packages/chess-types/src/messages.ts
@@ -1,6 +1,7 @@
 const messages = {
   INIT_GAME: "init_game",
-  GAME_STARTED: "game_started",
+  GAME_CREATED: "game_created",
+  SHOW_GAME_CREATED: "show_game_created",
   MAKE_MOVE: "make_move",
   SHOW_MOVE: "show_move",
   SHOW_GAME_OVER: "show_game_over",
