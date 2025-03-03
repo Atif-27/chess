b8c843dd299ff02507fe5317f20df943d8d5de6a
Atif <atif276ali@gmail.com>
2025-03-03 21:13:26 +0530

    fix(game_frontend): fixed middleware malfunction

--- apps/game_frontend/middleware.ts
+++ apps/game_frontend/middleware.ts
@@ -1,16 +1,14 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 export function middleware(request: NextRequest) {
-    const encryptedToken = request.cookies.get("token")?.value || "";
-    const redirectedResponse = NextResponse.redirect(
-      new URL("/login", request.url)
-    );
-    redirectedResponse.headers.set("x-middleware-cache", "no-cache");
-    if (!encryptedToken) return redirectedResponse;
+  const token = request.cookies.get("token")?.value;
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  return NextResponse.next();
 }
- 
-// See "Matching Paths" below to learn more
+
 export const config = {
-  matcher: '/game',
-}
+  matcher: "/game",
+};
